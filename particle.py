21b84dad509979cf165414e7cf4275d9cd470200
aakpat6 <aakpat6@gmail.com>
2013-04-17 23:15:19 -0400

    Added mass to particles

--- particle.py
+++ particle.py
@@ -1,8 +1,8 @@
 #This class will represent a point-particle and all the goodness therein
 class particle(object):
 
-   def __init__(self,x,y):
-      self.x = x;
-      self.y = y;
-
-#btw, im fairly positive this is completely wrong
+   def __init__(self,x,y,z,m):
+      self.x = x
+      self.y = y
+      self.z = z
+      self.mass = m
