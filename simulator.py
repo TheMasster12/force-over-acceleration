5ea42fdef69c15da9a905d0e6dd265beaf69e4bd
TheMasster12 <amass1212@gmail.com>
2013-04-18 13:41:22 -0500

    Added a third body.

--- simulator.py
+++ simulator.py
@@ -3,7 +3,7 @@ import random
 import body
 import vector
 
-NUM_BODIES = 100
+NUM_BODIES = 10 
 DIM_X = 1000
 DIM_Y = 1000
 DIM_Z = 1000
@@ -11,12 +11,11 @@ MASS_MIN = 1e-6
 MASS_MAX = 1e-3
 VELOCITY_MIN = 1e-2 
 VELOCITY_MAX = 100
-NUM_CYCLES = 2000;
+NUM_CYCLES = 10000
 
 def simulate():
-  #start graphics
-
   bodyArray = []
+
   for x in xrange(0,NUM_BODIES):
     ranX = random.random() * DIM_X
     ranY = random.random() * DIM_Y
@@ -29,31 +28,43 @@ def simulate():
     ranVelocity = vector.Vector(ranVx,ranVy,ranVz)
 
     bodyArray.append(body.Body(ranX,ranY,ranZ,ranMass,ranVelocity))
-
-  for x in xrange(0,NUM_CYCLES):
-    for i in range(0, len(bodyArray)):
-      for j in range(i+1,len(bodyArray)):
-        bodyArray[i].interactWith(bodyArray[j])
-    #paint
-  return
+  
+  for x in xrange(0,NUM_BODIES):
+		print(str(bodyArray[x])
+	
+	  for i in xrange(0, len(bodyArray)):
+			for j in xrange(i+1,len(bodyArray)):
+				bodyArray[i].interactWith(bodyArray[j])
+	
+	for x in xrange(0,NUM_BODIES):
+		print(str(bodyArray[x])
+  
+	return
 
 def simulate_test():
   bodyArray = []
   bodyArray.append(body.Body(500,500,0,5.972e24,vector.Vector(0,0,0)))
-  bodyArray.append(body.Body(500,500,6.371e6,1,vector.Vector(0,0,0)))
-  
+  bodyArray.append(body.Body(499,500,6.371e6,1,vector.Vector(0,0,0)))
+  bodyArray.append(body.Body(501,500,6.371e6,1,vector.Vector(0,0,0)))
+
   print(str(bodyArray[0]))
   print(str(bodyArray[1]))
+  print(str(bodyArray[2]))
 
   for x in xrange(0,NUM_CYCLES):
     bodyArray[0].interactWith(bodyArray[1])
     bodyArray[1].interactWith(bodyArray[0])
+    bodyArray[0].interactWith(bodyArray[2])
+    bodyArray[2].interactWith(bodyArray[0])
+    bodyArray[1].interactWith(bodyArray[2])
+    bodyArray[2].interactWith(bodyArray[1])
 
   print(str(bodyArray[0]))
   print(str(bodyArray[1]))
+  print(str(bodyArray[2]))
 
   return
 
 if __name__ == '__main__':
-  #simulate()
-  simulate_test()
+  #simulate_test()
+	simulate()
