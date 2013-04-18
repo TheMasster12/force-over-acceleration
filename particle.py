18d52314fb9c718526ba61077ce33c151601d9cb
TheMasster12 <amass1212@gmail.com>
2013-04-17 22:30:07 -0500

    Added kinetic energy and fixed class name

--- particle.py
+++ particle.py
@@ -1,8 +1,12 @@
 #This class will represent a point-particle and all the goodness therein
-class particle(object):
+class Particle(object):
 
-   def __init__(self,x,y,z,m):
+   def __init__(self,x,y,z,m,v):
       self.x = x
       self.y = y
       self.z = z
       self.mass = m
+      self.velocity = v
+
+   def getKineticEnergy():
+     return 0.5 * self.mass * self.velocity * self.velocity
