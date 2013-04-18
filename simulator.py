ecd0478267687959c755c38f2c3b43ce356132df
aakpat6 <aakpat6@gmail.com>
2013-04-18 00:00:00 -0400

    Modify simulator a little

--- simulator.py
+++ simulator.py
@@ -1,14 +1,15 @@
 #This class will be the backbone of the whole simulation.
-import random.py
+import random
+import body
 
-NUM_BODIES = 100;
-DIM_X = 1000;
-DIM_Y = 1000;
-DIM_Z = 1000;
-MASS_MIN = 0.000001 #1e-6 kg
-MASS_MAX = 0.001 #1e-3 kg
-VELOCITY_MIN = 0.01 #1e-2 m/s
-VELOCITY_MAX = 100 #100 m/s
+NUM_BODIES = 100
+DIM_X = 1000
+DIM_Y = 1000
+DIM_Z = 1000
+MASS_MIN = 1e-6
+MASS_MAX = 1e-3
+VELOCITY_MIN = 1e-2 
+VELOCITY_MAX = 100 
 
 def run_simulation():
   #start graphics
@@ -16,11 +17,11 @@ def run_simulation():
   bodyArray = []
   for x in range(0,NUM_BODIES):
     ranX = random.random() * DIM_X
-     ranY = random.random() * DIM_Y
-     ranZ = random.random() * DIM_Z
-     ranMass = random.uniform(MASS_MIN,MASS_MAX)
-     ranVelocity = random.uniform(VELOCITY_MIN,VELOCITY_MAX)
-     bodyArray.append(Body(ranX,ranY,ranZ,ranMass,ranVelocity))
+    ranY = random.random() * DIM_Y
+    ranZ = random.random() * DIM_Z
+    ranMass = random.uniform(MASS_MIN,MASS_MAX)
+    ranVelocity = random.uniform(VELOCITY_MIN,VELOCITY_MAX)
+    bodyArray.append(body.Body(ranX,ranY,ranZ,ranMass,ranVelocity))
 
   for i in range(0, bodyArray.length):
     for j in range(i, bodyArray.length):
