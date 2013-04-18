5cf84a7efc2e0edab71e3f0095f102dedc2be45b
TheMasster12 <amass1212@gmail.com>
2013-04-17 22:12:09 -0500

    Added some py files for fun and though

---
+++ particle.py
@@ -0,0 +1,8 @@
+#This class will represent a point-particle and all the goodness therein
+class particle(object):
+
+   def __init__(self,x,y):
+      self.x = x;
+      self.y = y;
+
+#btw, im fairly positive this is completely wrong
---
+++ simulator.py
@@ -0,0 +1,9 @@
+#This class will be the backbone of the whole simulation.
+
+def run_simulation():
+   #start graphics
+   #create point-particles
+   #loop through simulation
+      #paint
+   #finish
+   return
