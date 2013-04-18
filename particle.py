1a662dd5bd5bbdddc7bd639093c7037b085d9acc
aakpat6 <aakpat6@gmail.com>
2013-04-17 23:34:04 -0400

    Fix particles

--- particle.py
+++ particle.py
@@ -1,12 +1,14 @@
+import vectors
+
 #This class will represent a point-particle and all the goodness therein
 class Particle(object):
 
-   def __init__(self,x,y,z,m,v):
-      self.x = x
-      self.y = y
-      self.z = z
-      self.mass = m
-      self.velocity = v
+  def __init__(self,x,y,z,m,v):
+    self.x = x
+    self.y = y
+    self.z = z
+    self.mass = m
+    self.velocity = v # should be a Vector
 
-   def getKineticEnergy():
-     return 0.5 * self.mass * self.velocity * self.velocity
+  def kineticEnergy():
+    return 0.5 * self.mass * self.velocity.length() * self.velocity.length()
