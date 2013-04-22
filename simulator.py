fee8b2b982a0731bc5be14b3e873aa35ba2e6f03
TheMasster12 <amass1212@gmail.com>
2013-04-22 02:00:37 -0500

    It works! Yay

--- simulator.py
+++ simulator.py
@@ -45,7 +45,6 @@ def initFun():
   gluOrtho2D(0.0,1000.0,0.0,1000.0)
 
 def displayFun():
-  print("here")
   simulateFrame()
 
   glClear(GL_COLOR_BUFFER_BIT)
@@ -82,11 +81,12 @@ def simulate_test():
   return
 
 if __name__ == '__main__':
+  generateBodies()
   glutInit()
   glutInitWindowSize(1000,1000)
   glutCreateWindow("Force-Over-Acceleration")
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
   glutDisplayFunc(displayFun)
+  glutIdleFunc(displayFun) #Im pretty sure this is wrong
   initFun()
-  generateBodies()
   glutMainLoop()
