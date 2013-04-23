2a0e8f658df9650ad13128367b57d1c1b18ee758
TheMasster12 <amass1212@gmail.com>
2013-04-23 01:13:23 -0500

    Added q to quit and framerate now prints to the terminal

--- simulator.py
+++ simulator.py
@@ -1,8 +1,6 @@
 #This class will be the backbone of the whole simulation.
-import random,math
-
-import body
-import vector
+import random,math,time
+import body,vector
 
 from OpenGL.GL import *
 from OpenGL.GLUT import *
@@ -20,32 +18,36 @@ VELOCITY_MAX = 100
 CAM_DISTANCE = max([DIM_X,DIM_Y,DIM_Z]) * 1.5
 
 bodyArray = []
+frameCount = 0
+frameTimeHolder= int(round(time.time() * 1000))
 
 def generateRandomBodies():
+  global bodyArray
   for x in xrange(NUM_BODIES):
     ranX = random.random() * DIM_X - (DIM_X / 2)
     ranY = random.random() * DIM_Y - (DIM_Y / 2)
     ranZ = random.random() * DIM_Z - (DIM_Z / 2)
     ranMass = random.uniform(MASS_MIN,MASS_MAX)
-    
+
     '''
     ranVx = random.uniform(VELOCITY_MIN,VELOCITY_MAX)
     ranVy = random.uniform(VELOCITY_MIN,VELOCITY_MAX)
     ranVz = random.uniform(VELOCITY_MIN,VE100LOCITY_MAX)
     ranVelocity = vector.Vector(ranVx,ranVy,ranVz)
     '''
-
     ranVelocity = vector.zero()
 
     bodyArray.append(body.Body(ranX,ranY,ranZ,ranMass,ranVelocity))
-  
+
 def readBodies(args):
+  global bodyArray
   for arg in args:
     b = [float(x) for x in arg.split()]
     v = vector.Vector(b[4], b[5], b[6])
     bodyArray.append(body.Body(b[0], b[1], b[2], b[3], v))
 
-def simulateFrame():  
+def simulateFrame(): 
+  global bodyArray
   forces = [x.totalForceOn(bodyArray) for x in bodyArray] # Force on each body
   for i in xrange(len(bodyArray)):
     bodyArray[i].move(forces[i])
@@ -71,6 +73,7 @@ def initFun():
   gluLookAt(0.0, 0.0, CAM_DISTANCE, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
 
 def displayFun():
+  global bodyArray,frameCount,frameTimeHolder
   simulateFrame()
   glClear(GL_COLOR_BUFFER_BIT)
 
@@ -82,42 +85,30 @@ def displayFun():
 
   glFlush()
 
-def simulate_test():
-  bodyArray = []
-  bodyArray.append(body.Body(500,500,0,5.972e24,vector.Vector(0,0,0)))
-  bodyArray.append(body.Body(499,500,6.371e6,1,vector.Vector(0,0,0)))
-  bodyArray.append(body.Body(501,500,6.371e6,1,vector.Vector(0,0,0)))
+  frameCount += 1
+  if frameCount % 10 == 0:
+    print(1.0 / ((int(round(time.time() * 1000)) - frameTimeHolder) / 10000.0))
+    frameTimeHolder = int(round(time.time() * 1000)) 
 
-  print(str(bodyArray[0]))
-  print(str(bodyArray[1]))
-  print(str(bodyArray[2]))
-
-  for x in xrange(NUM_CYCLES):
-    bodyArray[0].interactWith(bodyArray[1])
-    bodyArray[1].interactWith(bodyArray[0])
-    bodyArray[0].interactWith(bodyArray[2])
-    bodyArray[2].interactWith(bodyArray[0])
-    bodyArray[1].interactWith(bodyArray[2])
-    bodyArray[2].interactWith(bodyArray[1])
-
-  print(str(bodyArray[0]))
-  print(str(bodyArray[1]))
-  print(str(bodyArray[2]))
-
-  return
+def handleKeypress(key,x,y):
+  if key == 'q':
+    sys.exit(0)
+  if key == 'r':
+    print("restart")
 
 if __name__ == '__main__':
-  print sys.argv
+  glutInit()
+  glutInitWindowSize(1000,1000)
+  glutCreateWindow("Force-Over-Acceleration")
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+  glutKeyboardFunc(handleKeypress)
+  glutDisplayFunc(displayFun)
+  glutIdleFunc(displayFun) #Im pretty sure this is wrong
+
   if len(sys.argv) < 2:
     generateRandomBodies()
   else:
     readBodies(sys.stdin.readlines())
 
-  glutInit()
-  glutInitWindowSize(1000,1000)
-  glutCreateWindow("Force-Over-Acceleration")
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
-  glutDisplayFunc(displayFun)
-  glutIdleFunc(displayFun) #Im pretty sure this is wrong
   initFun()
   glutMainLoop()
